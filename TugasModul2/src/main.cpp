b6b7db56dcb4a13818263573c4a80a6aac179fda
Naufal <16023154@mahasiswa.itb.ac.id>
2023-11-26 19:38:31 +0700

    Input fungsi drone + .gitignore

--- TugasModul2/src/main.cpp
+++ TugasModul2/src/main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
+#include <vector>
+
+using std::cout, std::cin, std::endl;
+
+float drone_x, drone_y = 0;
+std::vector<std::array<float, 2>> drone_history = {};
+
+std::string inputs[] = {
+    "gerak",
+    "lokasi",
+    "gerak_2",
+    "undo",
+    "redo",
+    "save",
+    "load",
+    "quit"
+};
 
 int main() {
-    
+    while (true) {
+        cout << "SIMULASI DRONE\nFungsi yang dapat digunakan:\n";
+
+        int inputs_len = sizeof(inputs)/sizeof(std::string);
+        for (int i = 0; i < inputs_len; i++) {
+            cout << i+1 << ". " << inputs[i] << endl;
+        }
+
+        int input;
+        cin >> input;
+
+        if (input < 0 || input > inputs_len) {
+            cout << "Bukan input yang benar!" << endl << endl;
+            continue;
+        }
+    }
 }
