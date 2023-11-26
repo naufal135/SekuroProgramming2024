5500a37fd89b09682714e1af7e8e5b28e629df7b
Naufal <16023154@mahasiswa.itb.ac.id>
2023-11-26 19:56:43 +0700

    gerak_2

--- TugasModul2/src/main.cpp
+++ TugasModul2/src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
+
+#define PI 3.14159265358
 
 using std::cout, std::cin, std::endl;
 
@@ -16,6 +19,15 @@ std::string inputs[] = {
     "load",
     "quit"
 };
+
+float ask_float_input(std::string name) {
+    float input;
+
+    cout << name << ": ";
+    cin >> input;
+
+    return input;
+}
 
 int main() {
     bool running = true;
@@ -39,25 +51,37 @@ int main() {
         cout << endl; //Mempermudah membaca
 
         switch (input) {
-            case 1: // Gerak
+            case 1: { // Gerak
                 float x, y;
 
-                cout << "X: ";
-                cin >> x;
-
-                cout << "Y: ";
-                cin >> y;
+                x = ask_float_input("x");
+                y = ask_float_input("y");
 
                 drone_x += x;
                 drone_y += y;
                 break;
-            case 2: // lokasi
+            }
+            case 2: { // lokasi
                 cout << "(" << drone_x << ", " << drone_y << ")" << endl;
                 system("pause");
                 break;
-            case 8: // quit
+            }
+            case 3: { // gerak_2
+                float v, t, theta;
+
+                v = ask_float_input("v");
+                t = ask_float_input("t");
+                theta = ask_float_input("theta");
+
+                float d = v * t;
+                drone_x += d * cos(theta * PI / 180);
+                drone_y += d * sin(theta * PI / 180);
+                break;
+            }
+            case 8: { // quit
                 running = false;
                 break;
+            }
         }
 
         cout << endl; //Mempermudah membaca
